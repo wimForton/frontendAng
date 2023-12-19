e180806c804959e3d0dbd74a1faf51e52827d2b5
wim forton <wim.forton@gmail.com>
2023-12-19 16:13:54 +0100

    hero list and edit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
+import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
